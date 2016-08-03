1fab7b3c31d239d259551642c666e6b4cb70592a
Anthony Turrigiano <anthony.turrigiano@gmail.com>
2016-08-03 14:32:34 +0000

    Add angular module, add link to index.html

---
+++ client/app/app.module.js
@@ -0,0 +1,23 @@
+/*
+ * We will wrap our code in a Immediately-Invoked Function Express or (IIFE) for short
+ * This will execute the code immediately after it is created
+ */
+(function(){
+    
+    /*
+     * The use strict is an expression that is used to make sure 
+     * that your code does not have undeclared variables
+     */
+    
+    "use strict";
+    
+    /*
+     * The Angular.module is a global place for creating, registering and retrieving
+     * Angular core or 3rd party modules
+     */
+    
+    angular
+        //module
+        .module( "myApp", []);
+        
+})();
